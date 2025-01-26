c6af56574de68eaee74cea98985052169c60a533
Devyn Johnson <dljohnson1313@gmail.com>
2025-01-26 10:58:33 -0600

    Work on index.js and files in classes to display desired data when using application. Installing cli-table3 npm package to have more control over data being displayed.

--- src/classes/employee.js
+++ src/classes/employee.js
@@ -11,7 +11,13 @@ class Employee { // Define the Employee class with constructor information to ma
     }
 
     async function viewEmployees() {
-        const query = 'SELECT * FROM employee';
+        const query = `
+            SELECT employee.id, employee.first_name, employee.last_name, role.title AS role_title, department.name AS department_name, role.salary, manager.first_name AS manager_first_name, manager.last_name AS manager_last_name
+            FROM employee
+            JOIN role ON employee.role_id = role.id
+            JOIN department ON role.department_id = department.id
+            LEFT JOIN employee AS manager ON employee.manager_id = manager.id
+        `;
         const result = await pool.query(query);
         return result.rows;
     }
